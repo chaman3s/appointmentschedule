51c76227ea9e9fe689cc59e9b5c90d9602401e0a
chaman3s <chamanggarwal@gmail.com>
2026-04-10 12:37:29 +0530

    update add .env

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -4,15 +4,19 @@ import { AuthServices } from './auth.service';
 import { AuthController } from './auth.Controller';
 import { UserModule } from '../users/user.module';
 import { DoctorModule } from '../doctors/doctors.module';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UserModule,
     DoctorModule,
-    JwtModule.register({
-      secret: 'mySecretKey',
-      signOptions: { expiresIn: '1d' },
-    }),
+    JwtModule.registerAsync({
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService) => ({
+    secret: configService.get<string>('JWT_SECRET'),
+    signOptions: { expiresIn: '1d' },
+  }),
+})
   ],
   controllers: [AuthController],
   providers: [AuthServices],
