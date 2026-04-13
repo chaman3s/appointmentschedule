ede98c9e9da359b5a9d33bf7bc8a0f93bb0ba228
chaman3s <chamanggarwal@gmail.com>
2026-04-13 12:33:05 +0530

    update make database config

--- backend/src/config/database.config.ts
+++ backend/src/config/database.config.ts
@@ -2,14 +2,14 @@ import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const getDatabaseConfig = (
-config: ConfigService,
+  config: ConfigService,
 ): TypeOrmModuleOptions => ({
-type: 'postgres',
-host: config.get<string>('DB_HOST'),
-port: parseInt(config.get<string>('DB_PORT')!),
-username: config.get<string>('DB_USER'),
-password: config.get<string>('DB_PASS'),
-database: config.get<string>('DB_NAME'),
-autoLoadEntities: true,
-synchronize: true,
+  type: 'postgres',
+  url: config.get<string>('DATABASE_URL'),
+  autoLoadEntities: true,
+  synchronize: false,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+  
